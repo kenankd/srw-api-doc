916875751ee20cb4c629aca389c110f87dd8c9c2
kenand <kenand@bicomsystems.com>
2025-01-30 08:38:37 +0100

    - Add SERVERware API documentation

---
+++ dist/swagger-initializer.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "openapi.yml",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    filter: true,
+    presets: [
+      SwaggerUIBundle.presets.apis
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ]
+  });
+
+  //</editor-fold>
+};
